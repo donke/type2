0491ab0bbf3d7b6ad7f708eb3ee44980af081b8a
donke <aka.kudoken@gmail.com>
2015-11-22 19:13:16 +0900

    Support the multiple character encoding

--- type2.go
+++ type2.go
@@ -1,13 +1,19 @@
 package type2
 
 import (
+	"io"
+	"io/ioutil"
 	"os"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 type Type2 struct {
 	Name     string
 	Typeable bool
-	File     *os.File
+	File     io.Reader
+	file     *os.File
 }
 
 func New(name string) *Type2 {
@@ -30,13 +36,38 @@ func New(name string) *Type2 {
 	if err != nil {
 		typeable = false
 	}
+	buf, err := ioutil.ReadAll(file)
+	if err != nil {
+		typeable = false
+	}
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		typeable = false
+	}
+	if !typeable {
+		return &Type2{
+			Name:     name,
+			Typeable: false,
+		}
+	}
+
+	var reader io.Reader
+	e := guess_jp(buf)
+	switch e {
+	case ShiftJis:
+		reader = transform.NewReader(file, japanese.ShiftJIS.NewDecoder())
+	case EucJp:
+		reader = transform.NewReader(file, japanese.EUCJP.NewDecoder())
+	default:
+		reader = file
+	}
 	return &Type2{
 		Name:     name,
 		Typeable: typeable,
-		File:     file,
+		File:     reader,
 	}
 }
 
 func (t2 *Type2) Close() {
-	t2.File.Close()
+	t2.file.Close()
 }
