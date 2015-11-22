b9eb1db1cc816acc40bcb98ced8d599da5a7bc14
donke <aka.kudoken@gmail.com>
2015-11-22 15:58:25 +0900

    Append test case about eucjp encoding

--- guess_test.go
+++ guess_test.go
@@ -4,10 +4,18 @@ import (
 	"testing"
 )
 
-func TestGuess(t *testing.T) {
+func TestGuessUTF8(t *testing.T) {
 	utf8 := "\x67\x6f\xe3\x81\xaf\xe6\xa5\xbd\xe3\x81\x97"
 	actual := guess_jp([]byte(utf8))
 	if actual != Utf8 {
+		t.Errorf("got %v", actual)
+	}
+}
+
+func TestGuessEUCJP(t *testing.T) {
+	eucjp := "\x67\x6f\xa4\xcf\xb3\xda\xa4\xb7"
+	actual := guess_jp([]byte(eucjp))
+	if actual != EucJp {
 		t.Errorf("got %v", actual)
 	}
 }
